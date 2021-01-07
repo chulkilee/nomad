a481ae0a1e24a1ff40b32d155b73cdb2b317883a
Drew Bailey <drewbailey5@gmail.com>
2021-01-07 15:17:14 -0500

    copy networks

--- scheduler/util.go
+++ scheduler/util.go
@@ -730,7 +730,7 @@ func inplaceUpdate(ctx Context, eval *structs.Evaluation, job *structs.Job,
 			Shared: structs.AllocatedSharedResources{
 				DiskMB:   int64(update.TaskGroup.EphemeralDisk.SizeMB),
 				Ports:    update.Alloc.AllocatedResources.Shared.Ports,
-				Networks: update.Alloc.AllocatedResources.Shared.Networks,
+				Networks: update.Alloc.AllocatedResources.Shared.Networks.Copy(),
 			},
 		}
 		newAlloc.Metrics = ctx.Metrics()
